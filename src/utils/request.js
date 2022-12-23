936b7ed774e2d8647bdcd555f2c5a6f54a2a9f30
ming <ming@acube.com.tw>
2022-12-23 18:00:59 +0800

    refactor(request): handleError

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,25 @@
 import Configuration from '@/configuration'
 import axios from 'axios'
-import errorHandle from './error-handle'
 import { useUser } from '@/stores/user'
 import { getToken } from '@/utils/auth'
+import useNotify from '@/use/useNotify'
+
+export const handleError = (error) => {
+  const { response } = error
+  const { notifyAPIError } = useNotify()
+  if (response) {
+    return Promise.reject(error)
+  }
+
+  // can not receive response
+  // 請求過時或者是斷網
+  if (!window.navigator.onLine) {
+    notifyAPIError({ message: '網絡有些問題。請重新加載' })
+  } else {
+    // maybe Program have some problem
+    return Promise.reject(error)
+  }
+}
 
 // create an axios instance
 const service = axios.create({
@@ -32,41 +49,8 @@ service.interceptors.request.use(
 )
 
 service.interceptors.response.use(
-  response => {
-    return response
-  },
-  error => {
-    const { response } = error
-    if (response) {
-      let message = error.message
-      if (error.response.data && error.response.data.message) {
-        message = error.response.data.message
-      } else if (error.response.data && error.response.data.errors) {
-        message = error.response.data.errors
-      } else if (error.response.data && error.response.data.error) {
-        message = error.response.data.error
-      }
-      // receive the response, but has error
-      errorHandle(response.status, message)
-      return Promise.reject(error)
-    }
-
-    // can not receive response
-    // 請求過時或者是斷網
-    if (!window.navigator.onLine) {
-      // Message(
-      //   {
-      //     showClose: true,
-      //     duration: 0,
-      //     message: '網絡有些問題。請重新加載',
-      //     type: 'error',
-      //   },
-      // )
-    } else {
-      // maybe Program have some problem
-      return Promise.reject(error)
-    }
-  },
+  response => response,
+  handleError,
 )
 
 export default service
