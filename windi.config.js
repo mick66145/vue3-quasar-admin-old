946cdd77c6050e6bda9498e610f86fd48164a004
ming <ming@acube.com.tw>
2023-01-04 13:11:41 +0800

    feat: windi screens config

--- windi.config.js
+++ windi.config.js
@@ -24,6 +24,13 @@ export default defineConfig({
         },
       },
     },
+    screens: {
+      xs: 0,
+      sm: '600px',
+      md: '1024px',
+      lg: '1440px',
+      xl: '1920px',
+    },
   },
   plugins: [
     // require('windicss/plugin/forms'),  // disable base input style
