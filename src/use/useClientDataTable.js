ec1a85256d68bf9aded8358a668ab45ec1355986
ming <ming@acube.com.tw>
2023-01-07 22:48:43 +0800

    refactor: useClientDataTable

--- src/use/useClientDataTable.js
+++ src/use/useClientDataTable.js
@@ -1,27 +1,43 @@
-import { useDataTableStore } from '@/stores/dataTable'
 import { reactive, ref, watch } from 'vue-demi'
+import useLocalStorage from './useLocalStorage'
 import XEUtils from 'xe-utils'
 import _ from 'lodash-es'
 export default function useClientDataTable ({
   searchParames = {},
+  localStorageKey = 'dashboardClientDataTable',
   tableFields = [],
 }) {
-  const store = useDataTableStore()
-  for (const [key, value] of Object.entries(searchParames)) {
-    !store.search[key] && (store.search[key] = value)
+  const { setLocalStorage, getLocalStorage } = useLocalStorage()
+  let localStorage = getLocalStorage(localStorageKey)
+  if (!localStorage) {
+    const localStorageObj = {
+      search: {
+        page: 1,
+        page_size: 10,
+      },
+    }
+    setLocalStorage(localStorageKey, localStorageObj)
+    localStorage = getLocalStorage(localStorageKey)
   }
+  for (const [key, value] of Object.entries(searchParames)) {
+    !localStorage.search[key] && (localStorage.search[key] = value)
+    key === 'page_size' && (localStorage.search[key] = value)
+  }
+  setLocalStorage(localStorageKey, localStorage)
 
-  const search = reactive(store.search)
-  const originalData = ref(store.data)
+  const search = reactive(localStorage.search)
+  const originalData = ref(localStorage.data)
   const data = ref([])
-  const total = ref(store.total)
+  const total = ref(0)
 
   const onChangePage = (page) => {
     search.page = page
+    setLocalStorage(localStorageKey, { search })
   }
 
   const onChangeFilter = () => {
     search.page = 1
+    setLocalStorage(localStorageKey, { search })
   }
 
   const onKeywordSearch = (keyword) => {
@@ -39,10 +55,12 @@ export default function useClientDataTable ({
     } else {
       data.value = originalData.value
     }
+    setLocalStorage(localStorageKey, { search, keyword: keyword })
   }
 
   const onReset = () => {
     search.page = 1
+    setLocalStorage(localStorageKey, { search })
   }
 
   // watch
