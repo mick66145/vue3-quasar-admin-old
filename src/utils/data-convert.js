913f33af19114497b26e882fd0598721cab05374
ming <ming@acube.com.tw>
2023-01-17 13:12:49 +0800

    feat(data-convert): 新增convertMoney

--- src/utils/data-convert.js
+++ src/utils/data-convert.js
@@ -3,3 +3,16 @@ import $dayjs from '@/plugins/dayjs'
 export const convertDateTime = (v, format = 'YYYY-MM-DD') => {
   return $dayjs(v).format(format)
 }
+
+export const convertMoney = (number, places, symbol, thousand, decimal) => {
+  number = number || 0
+  places = !isNaN(places = Math.abs(places)) ? places : 2
+  symbol = symbol !== undefined ? symbol : '$'
+  thousand = thousand || ','
+  decimal = decimal || '.'
+  var negative = number < 0 ? '-' : ''
+  var i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + ''
+  let j = ''
+  j = (j = i.length) > 3 ? j % 3 : 0
+  return symbol + negative + (j ? i.substr(0, j) + thousand : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : '')
+}
