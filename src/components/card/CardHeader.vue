471a8ebabf6b62d2f46384dbaa067f97d4d57e48
ming <mick66145@gmail.com>
2023-01-31 11:06:03 +0800

    feat: CardHeader

---
+++ src/components/card/CardHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-card-section class="mb-4 pb-0">
+    <div class="row items-center justify-between">
+      <div class="flex items-center">
+        <div class="text-h6 card-title">
+          <slot />
+        </div>
+      </div>
+      <div v-if="$slots.action">
+        <slot name="action" />
+      </div>
+    </div>
+  </q-card-section>
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+export default defineComponent({
+  setup () {
+    return {
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.card-title {
+  @apply px-11px relative;
+
+  &::before {
+    @apply content-[""];
+    @apply top-1/2 left-0 absolute;
+    @apply transform -translate-y-1/2;
+    @apply h-28px w-3px;
+    @apply bg-primary;
+  }
+}
+</style>
