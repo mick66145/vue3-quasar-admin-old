ee9ee97cede462297cdfbf8873ba5b3334009e63
ming <mick66145@gmail.com>
2023-02-02 16:13:51 +0800

    feat(permission): menu新增SidebarLink

--- src/layouts/components/sidebar/SidebarItem.vue
+++ src/layouts/components/sidebar/SidebarItem.vue
@@ -1,23 +1,17 @@
 <template>
   <div v-if="!item.hidden && item.children && !onlyOneChild?.noShowingChildren">
     <template v-if="onlyOneChild && !item.alwaysShow">
-      <q-item
+      <sidebar-link
         :to="resolvePath(onlyOneChild.path)"
-        active-class="q-item-no-link-highlighting"
-      >
-        <q-item-section avatar>
-          <q-icon :name="onlyOneChild.meta.icon" size="2rem" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ onlyOneChild.meta.title }}</q-item-label>
-        </q-item-section>
-      </q-item>
+        :title="onlyOneChild.meta.title"
+        :icon="onlyOneChild.meta.icon"
+      />
     </template>
 
     <q-expansion-item
       v-else
-      :icon="item.meta?.icon"
-      :label="item.meta?.title"
+      :icon="item.meta.icon"
+      :label="item.meta.title"
     >
       <q-list
         v-for="(childItem, childIndex) in visibleChildren"
@@ -30,15 +24,25 @@
           :item="childItem"
           :base-path="resolvePath(childItem.path)"
         />
+        <sidebar-link
+          v-else
+          :to="resolvePath(childItem.path)"
+          :title="childItem.meta.title"
+          :icon="childItem.meta.icon"
+        />
       </q-list>
     </q-expansion-item>
   </div>
 </template>
 
 <script>
+import SidebarLink from './SidebarLink.vue'
 import path from 'path'
 import { defineComponent, toRefs, computed } from 'vue-demi'
 export default defineComponent({
+  components: {
+    SidebarLink,
+  },
   props: {
     item: { type: Object },
     isNest: { type: Boolean, default: false },
@@ -64,7 +68,6 @@ export default defineComponent({
         }
         return !item.hidden
       })
-
       if (item.value.meta.slug === 'link') return oneChild
       if (item.value.meta.slug === 'dropdown' && showingChildren.length !== 0) return false
       if (item.value.meta.slug === 'dropdown' && showingChildren.length === 0) {
---
+++ src/layouts/components/sidebar/SidebarLink.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <q-item
+    :to="to"
+    active-class="q-item-no-link-highlighting"
+  >
+    <q-item-section avatar>
+      <q-icon :name="icon" size="2rem" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+export default defineComponent({
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String },
+    title: { type: String, default: '' },
+  },
+  setup () {
+    return {
+    }
+  },
+})
+</script>
