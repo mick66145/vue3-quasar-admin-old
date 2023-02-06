2f3071c22f104f5de778e39ddf9f60bdb44e0b5e
ming <mick66145@gmail.com>
2023-02-06 14:35:12 +0800

    feat(VxeClientTable、VxeServerTable): 新增checkox、footer相關fun

--- src/components/VxeClientTable.vue
+++ src/components/VxeClientTable.vue
@@ -2,6 +2,7 @@
   <div>
     <vxe-table
       ref="dataTable"
+      :key="refreshKey"
       class="q-mb-md"
       auto-resize
       round
@@ -9,6 +10,11 @@
       :row-config="{ isHover: true }"
       :data="data"
       :sort-config="{ trigger: 'cell'}"
+      :show-footer="showFooter"
+      :footer-span-method="footerSpanMethod"
+      :footer-method="footerMethod"
+      @checkbox-all="onCheckboxAll"
+      @checkbox-change="onCheckboxChange"
     >
       <slot />
     </vxe-table>
@@ -30,16 +36,62 @@ export default defineComponent({
     total: { type: Number, default: 0 },
     current: { type: Number, default: 1 },
     showPagination: { type: Boolean, default: true },
+    showFooter: { type: Boolean, default: false },
+    footerSpanMethod: { type: Function },
+    footerMethod: { type: Function },
   },
-  emits: ['update:current'],
+  emits: ['checkbox-all', 'checkbox-change', 'update:current'],
   setup (props, { emit }) {
+    // data
     const dataTable = ref()
+    const refreshKey = ref(0)
+
+    // methods
+    const refresh = () => {
+      refreshKey.value++
+    }
+    const updateFooter = () => {
+      dataTable.value.updateFooter()
+    }
+
+    // checkox相關methods
+    const getCheckboxRecords = () => {
+      return dataTable.value.getCheckboxRecords()
+    }
+    const setCheckboxRow = (rows, checked) => {
+      return dataTable.value.setCheckboxRow(rows, checked)
+    }
+    const toggleCheckboxRow = (row) => {
+      return dataTable.value.toggleCheckboxRow(row)
+    }
+    const setAllCheckboxRow = (checked) => {
+      return dataTable.value.setAllCheckboxRow(checked)
+    }
+    const clearCheckboxRow = () => {
+      return dataTable.value.clearCheckboxRow()
+    }
+    const onCheckboxAll = ({ checked }) => {
+      emit('checkbox-all', { checked })
+    }
+    const onCheckboxChange = ({ checked }) => {
+      emit('checkbox-change', { checked })
+    }
     const OnUpdateCurrent = (current) => {
       emit('update:current', current)
     }
 
     return {
       dataTable,
+      refreshKey,
+      refresh,
+      updateFooter,
+      getCheckboxRecords,
+      setCheckboxRow,
+      toggleCheckboxRow,
+      setAllCheckboxRow,
+      clearCheckboxRow,
+      onCheckboxAll,
+      onCheckboxChange,
       OnUpdateCurrent,
     }
   },
--- src/components/VxeServerTable.vue
+++ src/components/VxeServerTable.vue
@@ -2,6 +2,7 @@
   <div>
     <vxe-table
       ref="dataTable"
+      :key="refreshKey"
       class="q-mb-md"
       auto-resize
       round
@@ -9,7 +10,12 @@
       :row-config="{ isHover: true }"
       :data="data"
       :sort-config="{ trigger: 'cell',remote:true}"
-      @sort-change="OnChangeSort"
+      :show-footer="showFooter"
+      :footer-span-method="footerSpanMethod"
+      :footer-method="footerMethod"
+      @sort-change="onChangeSort"
+      @checkbox-all="onCheckboxAll"
+      @checkbox-change="onCheckboxChange"
     >
       <slot />
     </vxe-table>
@@ -17,7 +23,7 @@
       v-if="total > 0 && showPagination"
       :total="total"
       :current="current"
-      @update:current="OnUpdateCurrent"
+      @update:current="onUpdateCurrent"
     />
   </div>
 </template>
@@ -31,26 +37,69 @@ export default defineComponent({
     total: { type: Number, default: 0 },
     current: { type: Number, default: 1 },
     showPagination: { type: Boolean, default: true },
+    showFooter: { type: Boolean, default: false },
+    footerSpanMethod: { type: Function },
+    footerMethod: { type: Function },
   },
-  emits: ['sort-change', 'update:current'],
+  emits: ['sort-change', 'checkbox-all', 'checkbox-change', 'update:current'],
   setup (props, { emit }) {
+    // data
     const dataTable = ref()
+    const refreshKey = ref(0)
+
+    // methods
     const sort = (item) => {
       dataTable.value.sort(item)
     }
-    const OnChangeSort = ({ column, property, order, sortBy, sortList, $event }) => {
+    const refresh = () => {
+      refreshKey.value++
+    }
+    const updateFooter = () => {
+      dataTable.value.updateFooter()
+    }
+    const getCheckboxRecords = () => {
+      return dataTable.value.getCheckboxRecords()
+    }
+    const setCheckboxRow = (rows, checked) => {
+      return dataTable.value.setCheckboxRow(rows, checked)
+    }
+    const toggleCheckboxRow = (row) => {
+      return dataTable.value.toggleCheckboxRow(row)
+    }
+    const setAllCheckboxRow = (checked) => {
+      return dataTable.value.setAllCheckboxRow(checked)
+    }
+    const clearCheckboxRow = () => {
+      return dataTable.value.clearCheckboxRow()
+    }
+    const onChangeSort = ({ column, property, order, sortBy, sortList, $event }) => {
       emit('sort-change', { column, property, order, sortBy, sortList, $event })
     }
-
-    const OnUpdateCurrent = (current) => {
+    const onCheckboxAll = ({ checked }) => {
+      emit('checkbox-all', { checked })
+    }
+    const onCheckboxChange = ({ checked }) => {
+      emit('checkbox-change', { checked })
+    }
+    const onUpdateCurrent = (current) => {
       emit('update:current', current)
     }
 
     return {
       dataTable,
+      refreshKey,
       sort,
-      OnChangeSort,
-      OnUpdateCurrent,
+      refresh,
+      updateFooter,
+      getCheckboxRecords,
+      setCheckboxRow,
+      toggleCheckboxRow,
+      setAllCheckboxRow,
+      clearCheckboxRow,
+      onChangeSort,
+      onCheckboxAll,
+      onCheckboxChange,
+      onUpdateCurrent,
     }
   },
 })
