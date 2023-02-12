432d51996ec43407489fc0c15cb1cae7262536ec
ming <mick66145@gmail.com>
2023-02-12 17:42:43 +0800

    feat(AppHeader): 套上userinfo

--- src/layouts/components/AppHeader.vue
+++ src/layouts/components/AppHeader.vue
@@ -21,41 +21,56 @@
             @click="$q.fullscreen.toggle()"
           /> -->
         <lang-select />
-        <q-btn round flat>
-          <q-avatar size="26px">
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-          <q-menu
-            transition-show="scale"
-            transition-hide="scale"
-          >
-            <q-list class="min-w-100px">
-              <q-item
-                to="/profile"
-                clickable
-              >
-                <q-item-section>{{ $t('meun.profile') }}</q-item-section>
-              </q-item>
+        <q-item class="text-right">
+          <q-item-section>
+            <q-item-label>{{ userInfo.name }}</q-item-label>
+            <q-item-label caption lines="1">{{ userInfo.email }}</q-item-label>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-avatar>
+              <q-btn round flat>
+                <q-avatar size="44px">
+                  <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+                </q-avatar>
+                <q-menu
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-list class="min-w-100px">
+                    <q-item
+                      to="/profile"
+                      clickable
+                    >
+                      <q-item-section>{{ $t('meun.profile') }}</q-item-section>
+                    </q-item>
 
-              <q-separator />
-              <q-item clickable>
-                <q-item-section @click="logout">{{ $t('meun.logout') }}</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-btn>
+                    <q-separator />
+                    <q-item clickable>
+                      <q-item-section @click="logout">{{ $t('meun.logout') }}</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
+            </q-avatar>
+          </q-item-section>
+        </q-item>
       </div>
     </q-toolbar>
   </q-header>
 </template>
 
 <script>
-import { defineComponent } from 'vue-demi'
+import { defineComponent, reactive } from 'vue-demi'
 import { useApp } from '@/stores/app'
+import { useUser } from '@/stores/user'
 import useLogout from '@/use/useLogout'
 export default defineComponent({
   setup (props, { emit }) {
+    // data
     const storeApp = useApp()
+    const storeUser = useUser()
+    const userInfo = reactive(storeUser.info)
+
     // methods
     const toggle = () => {
       storeApp.toggleSideBar()
@@ -69,6 +84,7 @@ export default defineComponent({
     const { resetStore } = useLogout()
 
     return {
+      userInfo,
       toggle,
       logout,
     }
