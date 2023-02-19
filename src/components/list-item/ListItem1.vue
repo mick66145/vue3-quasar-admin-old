462ac95c7194fd5071dab8a17ee658125ad7794e
ming <mick66145@gmail.com>
2023-02-19 09:58:26 +0800

    feat: css deep

--- src/components/list-item/ListItem1.vue
+++ src/components/list-item/ListItem1.vue
@@ -1,24 +1,26 @@
 <template>
-  <q-card
-    class="border-primary"
-    :class="active && 'active'"
-    flat
-    bordered
-  >
-    <q-item class="px-7 q-my-lg">
-      <q-item-section avatar>
-        <q-icon :name="icon" :color="active?'white':'primary'" size="1.7rem" />
-      </q-item-section>
-      <q-item-section>
-        <q-item-label class="text-base">{{ title }}</q-item-label>
-        <q-item-label caption lines="1">{{ subtitle }}</q-item-label>
-      </q-item-section>
+  <div>
+    <q-card
+      class="border-primary"
+      :class="active && 'active'"
+      flat
+      bordered
+    >
+      <q-item class="px-7 q-my-lg">
+        <q-item-section avatar>
+          <q-icon :name="icon" :color="active?'white':'primary'" size="1.7rem" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-base">{{ title }}</q-item-label>
+          <q-item-label caption lines="1">{{ subtitle }}</q-item-label>
+        </q-item-section>
 
-      <q-item-section v-if="$slots.side" side>
-        <slot name="side" />
-      </q-item-section>
-    </q-item>
-  </q-card>
+        <q-item-section v-if="$slots.side" side>
+          <slot name="side" />
+        </q-item-section>
+      </q-item>
+    </q-card>
+  </div>
 </template>
 
 <script>
@@ -40,7 +42,7 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 :deep() {
-  .q-card {
+  &.q-card {
     &.active {
       @apply bg-primary;
 
