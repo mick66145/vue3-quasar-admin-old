f8e32c532117cd97894fa40f018b41d950317c9c
ming <mick66145@gmail.com>
2023-02-21 10:11:16 +0800

    feat: download-url directive

--- src/main.js
+++ src/main.js
@@ -5,24 +5,24 @@ import quasar from '@/plugins/quasar'
 
 // windi
 import 'virtual:windi.css'
-
 import App from './App.vue'
 import router from './router'
-import { useDayjs } from '@/plugins/dayjs'
 import GlobalComponents from './components'
-
 import 'virtual:svg-icons-register'
 
-// vxe-table
+// plugins
+import { useDayjs } from '@/plugins/dayjs'
 import vxeTable from '@/plugins/vxe-table'
-
 import editor from '@/plugins/editor'
+import i18n from '@/plugins/i18n'
+import { useVuelidate } from '@/plugins/vuelidate'
 
 import '@/styles/index.scss'
 
-import i18n from '@/plugins/i18n'
-import { useVuelidate } from '@/plugins/vuelidate'
+// directive
 import permission from '@/directive/permission'
+import downLoadUrl from '@/directive/download-url'
+
 import './permission'
 
 import { setupProdMockServer } from '../server/mock'
@@ -43,4 +43,5 @@ app.use(i18n)
 app.use(vxeTable)
 app.use(useVuelidate)
 app.use(permission)
+app.use(downLoadUrl)
 app.mount('#app')
