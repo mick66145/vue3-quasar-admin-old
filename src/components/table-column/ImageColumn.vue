806c481e717f5bff8f30831651fc8ee943d9b643
ming <mick66145@gmail.com>
2023-02-26 00:54:52 +0800

    feat: ImageColumn

---
+++ src/components/table-column/ImageColumn.vue
@@ -0,0 +1,35 @@
+<template>
+  <vxe-column
+    :field="field"
+    :title="title"
+    :sortable="sortable"
+    :width="width"
+  >
+    <template #default="{ row }">
+      <base-image
+        :src="getValueByPath(row,field)"
+      />
+    </template>
+  </vxe-column>
+</template>
+<script>
+
+import { defineComponent } from 'vue-demi'
+import { getValueByPath } from '@/utils'
+
+export default defineComponent({
+  props: {
+    field: { type: String },
+    title: { type: String },
+    sortable: { type: Boolean, default: false },
+    width: { type: Number, default: 200 },
+  },
+  setup () {
+    return {
+      getValueByPath,
+    }
+  },
+})
+</script>
+<style lang="scss" scoped>
+</style>
