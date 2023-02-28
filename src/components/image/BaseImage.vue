85091d416f1d57392463dfc91562425257e3cd3c
ming <mick66145@gmail.com>
2023-02-28 20:14:21 +0800

    feat(BaseImage): 新增預覽功能

--- src/components/image/BaseImage.vue
+++ src/components/image/BaseImage.vue
@@ -7,6 +7,8 @@
     :alt="alt"
     :height="height"
     :width="width"
+    :class="preview && 'cursor-pointer'"
+    @click="onPreview"
   >
     <template #error>
       <div class="bg-dark flex flex-center text-white absolute-full">
@@ -14,21 +16,31 @@
       </div>
     </template>
   </q-img>
+  <image-preview-dialog ref="dialog" />
 </template>
 
 <script>
-import { defineComponent } from 'vue-demi'
+import { defineComponent, ref } from 'vue-demi'
 
 export default defineComponent({
   props: {
     src: { type: String },
     ratio: { type: Number, default: 1 },
-    alt: { type: String, default: '' },
+    alt: { type: String },
     height: { type: String, default: '100%' },
     width: { type: String, default: '100%' },
+    preview: { type: Boolean, default: true },
   },
-  setup () {
+  setup (props) {
+    // data
+    const dialog = ref()
+
+    const onPreview = () => {
+      dialog.value.showDialog({ data: { image: { url: props.src } } })
+    }
     return {
+      dialog,
+      onPreview,
     }
   },
 })
