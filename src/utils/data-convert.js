882d3a1a2b1832d65eb0db941a99d5b40e7a4cce
ming <mick66145@gmail.com>
2023-03-01 14:37:17 +0800

    feat(data-convert): convertDateTime 預設改YYYY/MM/DD

--- src/utils/data-convert.js
+++ src/utils/data-convert.js
@@ -1,6 +1,6 @@
 import $dayjs from '@/plugins/dayjs'
 
-export const convertDateTime = (v, format = 'YYYY-MM-DD') => {
+export const convertDateTime = (v, format = 'YYYY/MM/DD') => {
   return $dayjs(v).format(format)
 }
 
