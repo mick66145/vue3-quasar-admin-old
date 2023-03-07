6b5a5e74b10196ba99d510e267722cc7741b244f
ming <mick66145@gmail.com>
2023-03-07 10:41:05 +0800

    feat: platformAttribute store

---
+++ src/stores/platformAttribute.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+import { PlatformAttributeResource } from '@/api'
+
+export const usePlatformAttribute = defineStore({
+  id: 'platformAttribute',
+  state: () => ({
+    platformAttributeResource: new PlatformAttributeResource(),
+    platformAttribute: '',
+  }),
+  actions: {
+    getPlatformAttribute () {
+      return new Promise((resolve, reject) => {
+        this.platformAttributeResource.get()
+          .then(res => {
+            this.platformAttribute = res
+            resolve()
+          }).catch(error => {
+            reject(error)
+          })
+      })
+    },
+  },
+
+})
