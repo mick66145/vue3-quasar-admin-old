c055b0594ad85fd59d35f8f0d757c30d86ac77dc
ming <mick66145@gmail.com>
2023-03-09 13:36:05 +0800

    feat: DownloadButton

---
+++ src/components/button/DownloadButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <base-button
+    color="positive"
+    :outline="outline"
+    :rounded="rounded"
+    :label="buttonLabel"
+    :icon="icon"
+  />
+</template>
+
+<script>
+import { defineComponent, computed, toRefs } from 'vue-demi'
+import { useI18n } from 'vue-i18n'
+export default defineComponent({
+  props: {
+    label: { type: String },
+    outline: { type: Boolean, default: false },
+    rounded: { type: Boolean, default: false },
+    icon: { type: String, default: 'arrow_downward' },
+    downloadUrl: { type: String },
+  },
+  setup (props) {
+    // data
+    const { t } = useI18n()
+    const { label } = toRefs(props)
+
+    // computed
+    const buttonLabel = computed(() => {
+      return label.value ? label.value : t('g.btn.dowload')
+    })
+
+    return {
+      buttonLabel,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
