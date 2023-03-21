9d7a415f12b12bd9c2696448390d0f3c74bf33c7
ming <mick66145@gmail.com>
2023-03-21 11:38:18 +0800

    feat: BaseLink

---
+++ src/components/link/BaseLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-btn
+    flat
+    padding="0"
+    :size="size"
+    :color="color"
+    :label="label"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+
+export default defineComponent({
+  props: {
+    label: { type: String, default: '我是連結' },
+    color: { type: String, default: 'blue' },
+    size: { type: String, default: '16px' },
+  },
+  setup () {
+    return {
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
