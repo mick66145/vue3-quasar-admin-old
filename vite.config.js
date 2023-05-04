3c22c0e89594b57db1f738ae92bb9cec21ec345c
ming <mick66145@gmail.com>
2023-05-04 16:27:49 +0800

    feat(env): dev、beta、production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable node/no-path-concat */
 import { fileURLToPath, URL } from 'url'
 import path, { resolve } from 'path-browserify'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import { viteMockServe } from 'vite-plugin-mock'
@@ -31,58 +31,68 @@ const localMockEnabled = process.env.USE_LOCAL_MOCK || true
 const prodMockEnabled = process.env.USE_PROD_MOCK || false
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    viteMockServe({
-      mockPath: './server/mock',
-      localEnabled: localMockEnabled, // 開發打包開關
-      prodEnabled: prodMockEnabled, // 生產打包開關
-      injectCode: 'setupProdMockServer()',
-      logger: false, // 是否在控制台顯示請求日誌
-      supportTs: false,
-    }),
-    vueJsx(),
-    WindiCSS(),
-    stylelint(),
-    createSvgIconsPlugin({
+const root = process.cwd()
+export default defineConfig(({ command, mode }) => {
+  let env = {}
+  const isBuild = command === 'build'
+  if (!isBuild) {
+    env = loadEnv((process.argv[3] === '--mode' ? process.argv[4] : process.argv[3]), root)
+  } else {
+    env = loadEnv(mode, root)
+  }
+  return {
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      viteMockServe({
+        mockPath: './server/mock',
+        localEnabled: localMockEnabled, // 開發打包開關
+        prodEnabled: prodMockEnabled, // 生產打包開關
+        injectCode: 'setupProdMockServer()',
+        logger: false, // 是否在控制台顯示請求日誌
+        supportTs: false,
+      }),
+      vueJsx(),
+      WindiCSS(),
+      stylelint(),
+      createSvgIconsPlugin({
       // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[name]',
-    }),
-    vueI18n({
-      compositionOnly: false,
-      include: path.resolve(__dirname, 'src/locales/**'),
-    }),
-    quasar({
-      sassVariables: 'src/styles/abstracts/quasar-variables.scss',
-    }),
-  ],
-  test: {
-    environment: 'happy-dom',
-  },
-  server: {
-    ...https(),
-    cors: true,
-    port: 3000,
-    host: true,
-    hmr: {
+        iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[name]',
+      }),
+      vueI18n({
+        compositionOnly: false,
+        include: path.resolve(__dirname, 'src/locales/**'),
+      }),
+      quasar({
+        sassVariables: 'src/styles/abstracts/quasar-variables.scss',
+      }),
+    ],
+    test: {
+      environment: 'happy-dom',
+    },
+    server: {
+      ...https(),
+      cors: true,
       port: 3000,
+      host: true,
+      hmr: {
+        port: 3000,
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      path: 'path-browserify',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        path: 'path-browserify',
+      },
     },
-  },
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, 'index.html'),
-      nested: resolve(__dirname, 'dist/index.html'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'dist/index.html'),
+      },
     },
-  },
+  }
 })
