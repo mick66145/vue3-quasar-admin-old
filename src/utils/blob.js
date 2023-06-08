4067b7bee15f6c5a49a4e6fe289225cb4dd621a2
ming <mick66145@gmail.com>
2023-06-08 09:45:50 +0800

    feat(blob): 新增fun
    1. 新增fetchBlobDataAsDataUrl
    2. 新增readFileAsDataUrl

--- src/utils/blob.js
+++ src/utils/blob.js
@@ -9,12 +9,33 @@ export const readFileAsText = (file) => {
   })
 }
 
+export const readFileAsDataUrl = (file) => {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onload = () => {
+      const responseData = reader.result
+      resolve(responseData)
+    }
+    reader.readAsDataURL(file)
+  })
+}
+
 export const fetchBlobData = (fileSrc, fileOption = null) => {
   return new Promise((resolve) => {
     fetch(fileSrc, fileOption)
       .then((res) => res.blob())
       .then((blob) => {
         resolve(readFileAsText(blob))
+      })
+  })
+}
+
+export const fetchBlobDataAsDataUrl = (fileSrc, fileOption = null) => {
+  return new Promise((resolve) => {
+    fetch(fileSrc, fileOption)
+      .then((res) => res.blob())
+      .then((blob) => {
+        resolve(readFileAsDataUrl(blob))
       })
   })
 }
