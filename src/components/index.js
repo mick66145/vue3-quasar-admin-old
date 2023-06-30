b88d5a681499d6fcda3b37c555dbf57212e2524c
ming <mick66145@gmail.com>
2023-06-30 17:38:29 +0800

    feat: InputBirthday

--- src/components/index.js
+++ src/components/index.js
@@ -23,6 +23,7 @@ import InputEmail from './form/InputEmail.vue'
 import InputCitySelect from './form/InputCitySelect.vue'
 import InputAreaSelect from './form/InputAreaSelect.vue'
 import InputAddress from './form/InputAddress.vue'
+import InputBirthday from './form/InputBirthday.vue'
 import BaseButton from './button/BaseButton.vue'
 import AddButton from './button/AddButton.vue'
 import DeleteButton from './button/DeleteButton.vue'
@@ -88,6 +89,7 @@ export default {
     app.component('InputCitySelect', InputCitySelect)
     app.component('InputAreaSelect', InputAreaSelect)
     app.component('InputAddress', InputAddress)
+    app.component('InputBirthday', InputBirthday)
     app.component('BaseButton', BaseButton)
     app.component('AddButton', AddButton)
     app.component('DeleteButton', DeleteButton)
