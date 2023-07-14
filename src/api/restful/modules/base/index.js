05216131e1d5da243a7227f82e91ceb7c2f309b6
ming <mick66145@gmail.com>
2023-07-14 11:47:40 +0800

    refactor(api): 調整index 使用路徑

--- src/api/restful/modules/base/index.js
+++ src/api/restful/modules/base/index.js
@@ -1,18 +1,18 @@
 export const exportBaseApiModules = () => {
   const modules = {}
-  modules.AreaResource = require('./restful/area').default
-  modules.AuthResource = require('./restful/auth').default
-  modules.CityResource = require('./restful/city').default
-  modules.CompanyJobResource = require('./restful/company-job').default
-  modules.CompanyResource = require('./restful/company').default
-  modules.FileResource = require('./restful/file').default
-  modules.RoleResource = require('./restful/role').default
-  modules.SystemResource = require('./restful/system').default
-  modules.UserResource = require('./restful/user').default
-  modules.MenuPermissionResource = require('./restful/menu-permission').default
-  modules.PlatformAttributeResource = require('./restful/platform-attribute').default
-  modules.MailLogResource = require('./restful/mail-log').default
-  modules.MeResource = require('./restful/me').default
-  modules.MailinfoResource = require('./restful/mailinfo').default
+  modules.AreaResource = require('./modules/area').default
+  modules.AuthResource = require('./modules/auth').default
+  modules.CityResource = require('./modules/city').default
+  modules.CompanyJobResource = require('./modules/company-job').default
+  modules.CompanyResource = require('./modules/company').default
+  modules.FileResource = require('./modules/file').default
+  modules.RoleResource = require('./modules/role').default
+  modules.SystemResource = require('./modules/system').default
+  modules.UserResource = require('./modules/user').default
+  modules.MenuPermissionResource = require('./modules/menu-permission').default
+  modules.PlatformAttributeResource = require('./modules/platform-attribute').default
+  modules.MailLogResource = require('./modules/mail-log').default
+  modules.MeResource = require('./modules/me').default
+  modules.MailinfoResource = require('./modules/mailinfo').default
   return modules
 }
