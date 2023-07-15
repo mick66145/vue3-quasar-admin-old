82ed924f5f0256a8afa4fe3b46efe7133f36a40f
ming <mick66145@gmail.com>
2023-07-15 08:36:49 +0800

    refactor(useLogout): 修正useRouter只能在setup中使用

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,8 +1,7 @@
 import { useUser } from '@/stores/user'
-import { useRouter } from 'vue-router'
+import router from '@/router'
 import useNotify from './useNotify'
 export default function useLogout () {
-  const router = useRouter()
   const { notify } = useNotify()
   const storeUser = useUser()
 
