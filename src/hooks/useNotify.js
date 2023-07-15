88992b3d75a636cc7abff9b4470e41998415a192
ming <mick66145@gmail.com>
2023-07-15 08:35:36 +0800

    refactor(useNotify): 調整Notify使用方式

--- src/hooks/useNotify.js
+++ src/hooks/useNotify.js
@@ -1,28 +1,26 @@
-import { useQuasar } from 'quasar'
+import { Notify } from 'quasar'
 
 export default function useNotify () {
-  const $q = useQuasar()
-
-  const notify = ({ message, type = 'info', timeout = 3000, position = 'top' }) => $q.notify({
+  const notify = ({ message, type = 'info', timeout = 3000, position = 'top' }) => Notify.create({
     message,
     type,
     timeout,
     position,
   })
 
-  const notifyAPIError = ({ message }) => $q.notify({
+  const notifyAPIError = ({ message }) => Notify.create({
     message,
     type: 'negative',
     position: 'top',
   })
 
-  const notifyError = ({ message }) => $q.notify({
+  const notifyError = ({ message }) => Notify.create({
     message,
     type: 'negative',
     position: 'top',
   })
 
-  const customNotify = ({ message, timeout = 3000, position = 'top', color = 'brown', textColor = 'white', icon = 'announcement' }) => $q.notify({
+  const customNotify = ({ message, timeout = 3000, position = 'top', color = 'brown', textColor = 'white', icon = 'announcement' }) => Notify.create({
     message,
     timeout,
     position,
