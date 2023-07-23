657c337dd0f6562c431a84986bbc85482200e087
ming <mick66145@gmail.com>
2023-07-23 12:59:49 +0800

    feat(BaseForm): 新增methods

--- src/components/form/BaseForm.vue
+++ src/components/form/BaseForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-form @submit="onSubmit" @reset="onReset">
+  <q-form ref="form" @submit="onSubmit" @reset="onReset">
     <template v-if="$slots.default" #default>
       <slot name="default" />
     </template>
@@ -7,12 +7,31 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue-demi'
+import { defineComponent, ref } from 'vue-demi'
 
 export default defineComponent({
   props: { },
   emits: ['submit', 'reset'],
   setup (props, { emit }) {
+    // data
+    const form = ref()
+
+    // methods
+    const validate = () => {
+      return form.value.validate()
+    }
+    const resetValidation = () => {
+      form.value.resetValidation()
+    }
+    const submit = (evt = null) => {
+      form.value.submit(evt)
+    }
+    const reset = (evt = null) => {
+      form.value.reset(evt)
+    }
+    const getValidationComponents = () => {
+      return form.value.getValidationComponents()
+    }
     const onSubmit = () => {
       emit('submit')
     }
@@ -20,6 +39,12 @@ export default defineComponent({
       emit('reset')
     }
     return {
+      form,
+      validate,
+      resetValidation,
+      submit,
+      reset,
+      getValidationComponents,
       onSubmit,
       onReset,
     }
