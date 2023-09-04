1cc2469099bc2177186bd64c03abd5006e5eb2d4
ming <mick66145@gmail.com>
2023-09-05 05:40:08 +0800

    feat: 刪除useFieldError、useFormatDate

--- src/hooks/useFormatDate.js
+++
@@ -1,8 +0,0 @@
-import { inject } from 'vue-demi'
-
-export default function useFormatDate (customFormat) {
-  const formatDate = inject('formatDate')
-  return {
-    formatDate: (value, format) => formatDate(value, format || customFormat),
-  }
-}
