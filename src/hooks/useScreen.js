9648ea597ec8d48bdd4a51732961703b8ba5809c
ming <mick66145@gmail.com>
2023-09-28 06:05:50 +0800

    feat(useScreen): 新增isScreenLarger

--- src/hooks/useScreen.js
+++ src/hooks/useScreen.js
@@ -1,11 +1,28 @@
 import { useQuasar } from 'quasar'
-import { ref, onMounted } from 'vue-demi'
+import { ref, computed, onMounted } from 'vue-demi'
 
 export default function useScreen () {
   // data
   const $q = useQuasar()
   const screenHeight = ref($q.screen.height)
   const screenWidth = ref($q.screen.width)
+
+  // computed
+  const isScreenLargerSm = computed(() => {
+    return $q.screen.gt.sm
+  })
+  const isScreenLargerMd = computed(() => {
+    return $q.screen.gt.md
+  })
+  const isScreenLargerLg = computed(() => {
+    return $q.screen.gt.lg
+  })
+  const isScreenLargerXl = computed(() => {
+    return $q.screen.gt.xl
+  })
+  const isScreenLargerXxl = computed(() => {
+    return $q.screen.gt.xxl
+  })
 
   onMounted(() => {
     window.addEventListener('resize', () => { setScreenHeight(); setScreenWidth() })
@@ -22,5 +39,10 @@ export default function useScreen () {
   return {
     screenHeight,
     screenWidth,
+    isScreenLargerSm,
+    isScreenLargerMd,
+    isScreenLargerLg,
+    isScreenLargerXl,
+    isScreenLargerXxl,
   }
 }
