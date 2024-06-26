8b27e9f05c7fa61d9974186a3a21493850eb73c5
ming <mick66145@gmail.com>
2024-06-26 11:52:32 +0800

    refactor(InputAutocomplete): 變成使用input-search

--- src/components/form/InputAutocomplete.vue
+++ src/components/form/InputAutocomplete.vue
@@ -1,22 +1,29 @@
 <template>
-  <q-select
-    v-model="observeValue"
-    :options="filterOptions"
-    :clearable="clearable"
-    :outlined="outlined"
-    :use-input="useInput"
-    :map-options="mapOptions"
-    :emit-value="emitValue"
-    :hide-selected="hideSelected"
-    :fill-input="fillInput"
-    :input-debounce="inputDebounce"
-    :transition-show="transitionShow"
-    :transition-hide="transitionHide"
-    :option-label="optionLabelFn"
-    :option-value="optionValueFn"
-    @filter="filterFn"
-    @clear="clearFn"
+  <input-search
+    v-model="searchValue"
+    class="full-width"
+    label="車牌"
+    placeholder="請輸入車牌"
+    @change="search"
   >
+    <q-menu
+      ref="menu"
+      no-parent-event
+      :max-height="maxHeight"
+    >
+      <q-list class="min-w-4rem w-full">
+        <q-item
+          v-for="(optionItem, optionIndex) in options"
+          clickable
+          v-close-popup
+          :key="optionIndex"
+          @click="select(optionItem)"
+        >
+          <q-item-section>{{ optionLabelFn(optionItem) }}</q-item-section>
+        </q-item>
+      </q-list>
+    </q-menu>
+
     <template v-if="$slots.default" #default>
       <slot name="default" />
     </template>
@@ -25,133 +32,117 @@
       <slot name="prepend" />
     </template>
 
-    <template v-if="$slots.append" #append>
-      <slot name="append" />
+    <template #append>
+      <q-icon name="search" />
     </template>
 
     <template v-if="$slots.before" #before>
       <slot name="before" />
     </template>
+
     <template v-if="$slots.after" #after>
       <slot name="after" />
     </template>
+
     <template v-if="$slots.label" #label>
       <slot name="label" />
     </template>
+
     <template v-if="$slots.error" #error>
       <slot name="error" />
     </template>
+
     <template v-if="$slots.hint" #hint>
       <slot name="hint" />
     </template>
+
     <template v-if="$slots.counter" #counter>
       <slot name="counter" />
     </template>
+
     <template v-if="$slots.loading" #loading>
       <slot name="loading" />
     </template>
-
-    <template v-if="$slots.selected" #selected>
-      <slot name="selected" />
-    </template>
-
-    <template v-if="$slots.beforeOptions" #before-options>
-      <slot name="before-options" />
-    </template>
-
-    <template v-if="$slots.afterOptions" #after-options>
-      <slot name="after-options" />
-    </template>
-
-    <template #no-option>
-      <q-item>
-        <q-item-section class="text-grey">
-          無此結果
-        </q-item-section>
-      </q-item>
-    </template>
-
-    <template v-if="$slots.selectedItem" #selected-item>
-      <slot name="selected-item" />
-    </template>
-
-    <template v-if="$slots.option" #option>
-      <slot name="option" />
-    </template>
-  </q-select>
+  </input-search>
 </template>
 
 <script>
-import { useVModel } from '@vueuse/core'
-import { defineComponent, ref, computed } from 'vue-demi'
-import { selectMatchItem } from '@/utils/filter'
+import { defineComponent, ref, computed, toRefs } from "vue-demi";
+import { useVModel } from "@vueuse/core";
+import useCRUD from "@/hooks/useCRUD";
+
 export default defineComponent({
   props: {
     modelValue: { type: [String, Number, null, Object] },
-    options: { type: Array, default () { return [] } },
-    clearable: { type: Boolean, default: true },
-    outlined: { type: Boolean, default: true },
-    useInput: { type: Boolean, default: true },
-    mapOptions: { type: Boolean, default: true },
     emitValue: { type: Boolean, default: false },
-    fillInput: { type: Boolean, default: true },
-    inputDebounce: { type: Number, default: 0 },
-    transitionShow: { type: String, default: 'scale' },
-    transitionHide: { type: String, default: 'scale' },
-    optionLabel: { type: String, default: 'name' },
-    optionValue: { type: String, default: 'id' },
+    transitionShow: { type: String, default: "scale" },
+    transitionHide: { type: String, default: "scale" },
+    optionLabel: { type: String, default: "name" },
+    optionValue: { type: String, default: "id" },
+    maxHeight: { type: String, default: "300px" },
+    filterFetch: { type: Function },
+    filterQuery: { type: [String, null, Object] },
+    filterKey: { type: [Array], default: ["keyword"] },
   },
-  emits: [
-    'update:modelValue',
-  ],
-  setup (props, { emit }) {
+  emits: ["update:modelValue"],
+  setup(props, { emit }) {
     // data
-    const filterOptions = ref(props.options)
-    const observeValue = useVModel(props, 'modelValue', emit)
+    const menu = ref();
+    const { filterFetch, filterQuery, filterKey } = toRefs(props);
+    const observeValue = useVModel(props, "modelValue", emit);
+    const options = ref([]);
 
     // computed
-    const hideSelected = computed(() => {
-      return props.useInput
+    const searchValue = computed({
+      get () {
+        return observeValue.value ? optionValueFn(observeValue.value) : null
+      },
+      set (value) {
+        emit('update:modelValue', value);
+      },
     })
-
+    
     // methods
-    const filterFn = (val, update, abort) => {
-      setTimeout(() => {
-        update(() => {
-          if (val || observeValue.value) {
-            const needle = val.toLowerCase()
-            const needleOptions = selectMatchItem(props.options.map(item => item[props.optionLabel]), needle)
-            filterOptions.value = props.options.filter(item => needleOptions.includes(item[props.optionLabel]))
-          } else {
-            filterOptions.value = []
-          }
-        })
-      }, 500)
-    }
-    const clearFn = (val) => {
-      observeValue.value = null
-    }
+    const filterFn = async () => {
+      if (filterFetch.value) {
+        const keyObj = filterKey.value.reduce((obj, element) => { obj[element] = observeValue.value;
+          return obj;
+        },{});
+        const query = {...filterQuery.value,...keyObj};
+        const [res] = await callReadListFetch(query);
+        if (res.list.length > 0) {
+          options.value = [];
+          options.value = res.list;
+          menu.value.show();
+        }
+      }
+    };
     const optionValueFn = (item) => {
-      return item[props.optionValue] ? item[props.optionValue] : null
+      return (item[props.optionValue] !== undefined || null) ? item[props.optionValue] : item
     }
     const optionLabelFn = (item) => {
-      return item[props.optionLabel] ? item[props.optionLabel] : null
+      return (item[props.optionLabel] !== undefined || null) ? item[props.optionLabel] : item
     }
+    const search = () => filterFn();
+    const select = (item) => emit('update:modelValue',item);
+    
+    // use
+    const { callReadListFetch } = useCRUD({
+      readListFetch: filterFetch.value,
+    });
+
     return {
+      menu,
       observeValue,
-      filterOptions,
-      hideSelected,
-      filterFn,
-      clearFn,
-      optionValueFn,
+      searchValue,
+      options,
+      search,
+      select,
       optionLabelFn,
-    }
+    };
   },
-})
+});
 </script>
 
-<style lang="scss" scoped>
-.q-select {
-  @apply py-10px;
-}
+<style lang="postcss" scoped>
 </style>
